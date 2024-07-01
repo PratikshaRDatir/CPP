da26cb41efa4e84f98da47f13573467cbf96e18f
Pratiksha Datir@DESKTOP-E243SNP <pratikshadatir8@gmail.com>
2024-07-01 22:34:15 +0530

    EvenOddNumber

---
+++ EvenOddNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //To find the even no.
+    int a;
+    cout<<"Enter a no.\n";
+    cin>>a;
+    if(a%2==0)
+    {
+       cout<<"It is an EVEN no.";
+    }
+    else
+    {
+         cout<<"It is an ODD no.";
+    }
+    
+}
